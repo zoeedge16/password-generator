285e3ccaa62e1a4fd2ac03122ff9fdfd5a16cae3
Zoe  Edge <zoemedge@Zoes-MacBook-Pro.local>
2023-05-02 21:50:36 -0500

    adding my series of prompts and an if statement for charLength

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,27 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
+// series of prompts 
     var result = prompt("Please enter a value between 8-128.");
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    
-    var random = Math.floor(Math.random() * lowercase.length);
+    var lowercase = prompt('Would you like to use lowercase letters in your password? Y = yes N = no')
+    var uppercase = prompt('Would you liek to use uppercase letters in your password? Y = yes N = no')
+    var numeric = prompt('Would you liek to use numbers in your password? Y = yes N = no')
+    var specialCharacters = prompt('Would you liek to use special characters in your password? Y = yes N = no')
 
-    console.log("RANDOM", random, lowercase[random]);
+if (charLength < '8' && charLength > '128') {
+    alert('Choose a character length between 8 & 128.')
+    return generatePassword;
+};
+
+    // var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    
+//     var random = Math.floor(Math.random() * lowercase.length);
+
+//     console.log("RANDOM", random, lowercase[random]);
     
 
-    return "";
+//     return "";
 }
 
 generatePassword()
