c5d667fa2f614bf8b0878df22488740897d83cd3
zoe <zoeedge0703@gmail.com>
2023-09-16 17:24:47 -0500

    added all the variables, if statements, and for loops needed to execute the password generator

--- assets/script.js
+++ assets/script.js
@@ -3,29 +3,40 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-// series of prompts 
-    var result = prompt("Please enter a value between 8-128.");
-    var lowercase = prompt('Would you like to use lowercase letters in your password? Y = yes N = no')
-    var uppercase = prompt('Would you liek to use uppercase letters in your password? Y = yes N = no')
-    var numeric = prompt('Would you liek to use numbers in your password? Y = yes N = no')
-    var specialCharacters = prompt('Would you liek to use special characters in your password? Y = yes N = no')
-
-if (charLength < '8' && charLength > '128') {
-    alert('Choose a character length between 8 & 128.')
-    return generatePassword;
-};
-
-    // var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    
-//     var random = Math.floor(Math.random() * lowercase.length);
-
-//     console.log("RANDOM", random, lowercase[random]);
+    var passwordLength = (prompt("Please enter a value between 8-128."));
     
 
-//     return "";
+    if (!passwordLength || passwordLength < 8 || passwordLength > 128) {
+        alert('Choose a character length between 8 & 128.');
+        return '';
+    };
+
+    var includeLowercase = confirm('Include Lowercase letters?');
+    var includeUppercase = confirm('Include Uppercase letters?');
+    var includeNumeric = confirm('Include numeric characters?');
+    var includeSpecial = confirm('Include special characters?');
+
+    if (!includeLowercase && !includeNumeric && !includeSpecial && !includeUppercase) {
+        alert('At least one character type should be selected');
+        return '';
+    }
+
+    var allCharacters = '';
+    if (includeLowercase) allCharacters += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeNumeric) allCharacters += '0123456789';
+    if (includeSpecial) allCharacters += '!@#$%^&*()_-+[]{};:<>?';
+    if (includeUppercase)  allCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    var generatedPassword = '';
+    for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * allCharacters.length);
+        generatedPassword += allCharacters.charAt(randomIndex);
+    }
+
+return generatedPassword;
+
 }
 
-generatePassword()
 
 // Write password to the #password input
 function writePassword() {
